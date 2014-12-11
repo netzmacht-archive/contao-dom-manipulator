b62c65351f6fd3ed2cbb2c6ddb40a2b16ecaa0d5
David Molineus <mail@netzmacht.de>
2014-12-11 13:14:42 +0100

    Add get rules event

---
+++ src/Netzmacht/Contao/DomManipulator/Event/GetRulesEvent.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @package    contribute
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\DomManipulator\Event;
+
+use Netzmacht\DomManipulator\RuleInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class GetRulesEvent is emitted when the template listener collects are rules which should be applied.
+ *
+ * @package Netzmacht\Contao\DomManipulator\Event
+ */
+class GetRulesEvent extends Event
+{
+    const NAME = 'dom-manipulator.get-rules';
+
+    /**
+     * Collection of rules.
+     *
+     * @var array
+     */
+    private $rules = array();
+
+    /**
+     * Name of the template.
+     *
+     * @var string
+     */
+    private $templateName;
+
+    /**
+     * Construct.
+     *
+     * @param string $templateName Name of template being parsed.
+     */
+    function __construct($templateName)
+    {
+        $this->templateName = $templateName;
+    }
+
+    /**
+     * @param RuleInterface $rule     Rule being added.
+     * @param int           $priority Define a priority.
+     *
+     * @return $this
+     */
+    public function addRule(RuleInterface $rule, $priority = 0)
+    {
+        $this->rules[$priority][] = $rule;
+
+        return $this;
+    }
+
+    /**
+     * Add multiple rules.
+     *
+     * @param RuleInterface[] $rules    List of rules.
+     * @param int             $priority Priority is used for each rule.
+     *
+     * @return $this
+     */
+    public function addRules(array $rules, $priority = 0)
+    {
+        foreach ($rules as $rule) {
+            $this->addRule($rule, $priority);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get rules prioritized.
+     *
+     * @return RuleInterface[]
+     */
+    public function getRules()
+    {
+        $rules = array();
+
+        ksort($this->rules);
+
+        foreach ($this->rules as $prioritized) {
+            $rules = array_merge($rules, $prioritized);
+        }
+
+        return $rules;
+    }
+}
